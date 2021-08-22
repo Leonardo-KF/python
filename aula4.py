a619841dcce8465a915bd3a1c6975f03f5edc47a
Gabriel <gabrielsfarias@outlook.com>
2021-08-22 19:27:03 -0300

    add for

--- aula4.py
+++ aula4.py
@@ -2,8 +2,15 @@
 # menos de 3 anos, o ingresso será gratuito; entre 3 e 12 anos o ingresso custará R$15; se tiver mais de 12
 # anos, custará R$30.
 
-# Escreva um laço em que você pergunte a idade aos usuários, e então lhes informe o preço do do ingresso;
+# Escreva um laço em que você pergunte a idade aos usuários, e então lhes informe o preço do ingresso;
 # Encerre o laço usando um break quando o usuário digitar 'sair'.
 # Após encerrar o laço, apresente na tela o total de pessoas que compraram ingressos, o total de dinheiro
 # arrecadado e a média de idade das pessoas.
 
+ingresso = [0,15,30]
+counter = 0
+
+for i in ingresso:
+    pergunta = int(input('Qual sua idade: '))
+    if pergunta < 3:
+        print('')
