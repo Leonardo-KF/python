9842ae37eaec88bf730ffd8816c7ba5afd5e3a81
Gabriel <gabrielsfarias@outlook.com>
2021-08-23 04:16:48 -0300

    recomeça do inicio

--- challenge3.py
+++ challenge3.py
@@ -14,18 +14,27 @@
 #E se fosse para 17 mil pessoas, quantas pessoas gostariam de ver shows de apenas um palco?
 
 def retorna_pessoas_preferem_um_unico_palco(quantidade_pessoas_evento):
-    perc_total_pessoas = 100
-    perc_palco_A = 10
-    perc_palco_B = 6
-    perc_palco_C = 19
-    perc_palco_AB = 17
-    perc_palco_AC = 15
-    perc_palco_BC = 7
-    perc_palco_ABC = 3
-    perc_nenhum_palco = 23
-    perc_apenas_um = 35
-    ingressos = input('Quantos ingressos? ')
-    if ingressos == 10000:
-        print(perc_apenas_um*10000//100)
 
-retorna_pessoas_preferem_um_unico_palco(10000)            
+
+
+
+
+
+
+
+
+#    perc_total_pessoas = 100
+#    perc_palco_A = 10
+#    perc_palco_B = 6
+#    perc_palco_C = 19
+#    perc_palco_AB = 17
+#    perc_palco_AC = 15
+#    perc_palco_BC = 7
+#    perc_palco_ABC = 3
+#    perc_nenhum_palco = 23
+#    perc_apenas_um = 35
+#    ingressos = input('Quantos ingressos? ')
+#    if ingressos == 10000:
+#        print(perc_apenas_um*10000//100)
+#
+#retorna_pessoas_preferem_um_unico_palco()
