ab8542839e2b01738c275fad93bc209965c57e60
Gabriel <gabrielsfarias@outlook.com>
2021-08-24 13:11:22 -0300

    renomeia arquivo reinicia o codigo

---
+++ trabalho-pratico/exercicio1.py
@@ -0,0 +1,6 @@
+nomeAluno = input('Digite o nome do aluno: ')
+notaFinal = input('Digite a nota final do aluno: ')
+
+print(f'Nome do aluno: {nomeAluno}')
+print(f'Nota final: {notaFinal}')
+print(f'O aluno {nomeAluno} tirou {notaFinal:.1f} e se enquadra no conceito X')
